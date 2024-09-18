d887179e50ea484c5272c5369292f87a4c31169c
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-09-18 19:42:29 +0100

    Common root path in PingController

--- src/main/java/com/zuzanna/apidemo/controller/PingController.java
+++ src/main/java/com/zuzanna/apidemo/controller/PingController.java
@@ -1,20 +1,18 @@
 package com.zuzanna.apidemo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("ping")
 public class PingController {
-    @RequestMapping(path = "/ping", method = RequestMethod.POST)
+    @GetMapping("first")
     public PingResponse ping() {
         var response = new PingResponse();
         response.setMessage("Moge is cool!");
         return response;
     }
 
-    @RequestMapping(path = "/poo/bum", method = RequestMethod.GET)
+    @GetMapping("second")
     public PingResponse pingPing() {
         var response = new PingResponse();
         response.setMessage("Everyone is ace!");
