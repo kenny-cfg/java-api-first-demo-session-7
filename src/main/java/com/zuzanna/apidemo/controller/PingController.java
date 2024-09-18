5a11ced3b1318192e16359db1f66b0b37ad4d349
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-09-18 19:38:58 +0100

    Change to a POST

--- src/main/java/com/zuzanna/apidemo/controller/PingController.java
+++ src/main/java/com/zuzanna/apidemo/controller/PingController.java
@@ -7,14 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingController {
-    @RequestMapping(path = "/ping", method = RequestMethod.GET)
+    @RequestMapping(path = "/ping", method = RequestMethod.POST)
     public PingResponse ping() {
         var response = new PingResponse();
         response.setMessage("Moge is cool!");
         return response;
     }
 
-    @RequestMapping(path = "/ping/ping", method = RequestMethod.GET)
+    @RequestMapping(path = "/poo/bum", method = RequestMethod.GET)
     public PingResponse pingPing() {
         var response = new PingResponse();
         response.setMessage("Everyone is ace!");
