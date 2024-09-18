1d4d496b91549f50a77ab66580866855c60ef9f4
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-09-18 19:23:10 +0100

    Use @RestController

--- src/main/java/com/zuzanna/apidemo/controller/PingController.java
+++ src/main/java/com/zuzanna/apidemo/controller/PingController.java
@@ -1,14 +1,13 @@
 package com.zuzanna.apidemo.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class PingController {
     @RequestMapping(path = "/ping", method = RequestMethod.GET)
-    @ResponseBody
     public String ping() {
         return "pong";
     }
